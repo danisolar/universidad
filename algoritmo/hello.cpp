899b227f0dfb3fa27d22ee26c4b54f4c919c2034
danisolar <danisolar@live.cl>
2015-04-03 18:36:34 -0300

    hola mundo

---
+++ algoritmo/hello.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//string SALUDO_GLOBAL = "GLOBAL !!!!" /* global, accesible desde cualquier parte del programa*/
+
+int main() /*Función principal del programa*/
+{
+    printf("\nHELLO WORLD\n");
+    
+    printf("\npaso 1\n");
+    
+   
+    printf("\npaso 2\n");
+        
+    printf("\npaso 3\n");
+ 
+   getchar();
+ return(0);
+}
